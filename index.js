3e501988edb9dc75dc5bf186b083038d2195f512
Thomas Bowen <tbowen1@outlook.com>
2017-10-21 20:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,4 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function sayHiToGrandma(string) {
-  return ('YESINDEED').tolowercase()
-}
-function shout(string) {
-  console.log(toUpperCase)
-}
+
